b233d774f17230c81c2b522ad9d8ab4b24c1dd3e
wajid-ashraf <vajid.web@gmail.com>
2020-07-10 20:52:51 -0700

    code update

---
+++ script/action.js
@@ -0,0 +1,16 @@
+
+
+$('.second--nav .nav-link').click(function (e) {
+  $('.second--nav .nav-link').removeClass('current');
+  let havclass = $(e.target).hasClass('.current');
+  if (havclass == false) {
+    $(e.target).addClass('current');
+  }
+});
+$('#navbarNav .nav-link').click(function (e) {
+  $('#navbarNav .nav-link').removeClass('activ');
+  let havclass = $(e.target).hasClass('.activ');
+  if (havclass == false) {
+    $(e.target).addClass('activ');
+  }
+});
